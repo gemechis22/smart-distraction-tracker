4f9e4ab87ef32915d4376b5ca862943e84fea644
gemechis22 <gemechiskebeta@gmail.com>
2025-05-30 20:45:55 -0400

    Day 4: Retrieved and displayed current tab URL using Chrome Tabs API

--- extension/popup.js
+++ extension/popup.js
@@ -2,7 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const startButton = document.getElementById("start");
   const resetButton = document.getElementById("reset");
   const statusText = document.getElementById("status");
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  const tab =tabs[0];
+  const url = tab.url;
+  const statusText = document.getElementById("status");
 
+  statusText.innerHTML = `your're currently on:\n${url} `;
+
+  const title = tab.title;
+  statusText.innerText = ` You're on:\n${title}\n${url}`;
+  
+});
   startButton.addEventListener("click", () => {
     statusText.innerText = " Tracking started!";
   });
